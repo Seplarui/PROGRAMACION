f58705ca7de180556bb9e5e3d953a260a8a17c60
Sebas SEbas <seplarui@gmail.com>
2016-05-24 19:38:46 +0200

    TipoBoolean

---
+++ src/programacion/TipoBoolean.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion;
+
+/**
+ *
+ * @author sepla
+ */
+public class TipoBoolean {
+    
+    public static void main(String[] args) {
+        
+        boolean valor1;
+        valor1=true;
+        if (valor1) {
+            System.out.println("Valor1 = Verdadero");
+            
+        } else {
+            System.out.println("Valor1 = Falso");
+        }
+    }
+    
+}
