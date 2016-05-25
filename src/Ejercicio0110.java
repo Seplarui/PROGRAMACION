db6c53f98673b24036336da51b31f19d4b3eca38
Sebas <seplarui@gmail.com>
2016-05-25 12:11:56 +0200

    Ejercicio0110

---
+++ src/Ejercicio0110.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haplo
+ */
+import java.math.*;
+public class Ejercicio0110 {
+    
+    public static void main(String[] args) {
+        
+        double area=0;
+        area=5.2*5.2*(Math.PI);
+        System.out.println("El área es: "+area);
+        
+    }
+    
+}
