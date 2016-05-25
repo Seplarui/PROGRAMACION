164b533625ca14001ba05674e22e85d0d2bedb37
Sebas <seplarui@gmail.com>
2016-05-25 10:04:37 +0200

    Ejercicio0106

---
+++ src/Ejercicio0106.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haplo
+ */
+public class Ejercicio0106 {
+    
+    public static void main(String[] args) {
+        
+        int numero=2;
+        int cuad=numero*numero;
+        System.out.println("El cuadrado de "+numero+" es: " + cuad);
+    }
+    
+}
