7759643c6c3894c3483d3743e5dfacd44b8abbdc
Sebas <seplarui@gmail.com>
2016-05-25 10:00:14 +0200

    Ejercicio0104

---
+++ src/programacion/Ejercicio0104.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion;
+
+/**
+ *
+ * @author haplo
+ */
+public class Ejercicio0104 {
+    
+            static int n1=50;
+
+    
+    public static void main(String[] args) {
+        
+        int n2=30, suma=0;
+                int n3=0;
+        suma=n1+n2;
+        System.out.println("La suma es: " +suma);
+        suma=suma+n3;
+        System.out.println(suma);
+        
+        
+    }
+    
+}
