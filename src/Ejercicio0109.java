53723ce57a7f7c5e45abaf89022ed926be43f82d
Sebas <seplarui@gmail.com>
2016-05-25 12:13:34 +0200

    Ejercicio0109

---
+++ src/Ejercicio0109.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haplo
+ */
+public class Ejercicio0109 {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("Me gusta la programación\ncada día más");
+    }
+    
+}
