70a1aa19b2489e2e5a4f3b1bc84dc69698f78385
Sebas SEbas <seplarui@gmail.com>
2016-05-29 13:22:57 +0200

    ifelse

---
+++ src/ifelse.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sepla
+ */
+public class ifelse {
+    
+    public static void main(String[] args) {
+        int nota=6;
+        
+        if(nota<5) System.out.println("Mal");
+        if(nota==5) System.out.println("Regular");
+        if (nota>5) System.out.println("Bien");
+    }
+    
+}
