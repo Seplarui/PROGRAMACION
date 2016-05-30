287242add0b203a86d63b2a968d7b05770596743
Sebas <seplarui@gmail.com>
2016-05-30 10:06:12 +0200

    Ejercicio0310

---
+++ src/Ejercicio0311.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haplo
+ */
+public class Ejercicio0311 {
+    
+    public static void main(String[] args) {
+        int i=0;
+        
+        for (i=0;i<=50;i++){
+            if(i%2==0) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+}
