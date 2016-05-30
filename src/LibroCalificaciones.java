efcfae3460a390d1de887152edaa4b1c1ac3f86e
Sebas SEbas <seplarui@gmail.com>
2016-05-30 20:39:29 +0200

    LibroCalificaciones

---
+++ src/LibroCalificaciones.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sepla
+ */
+public class LibroCalificaciones {
+    
+    public void mostrarMensaje() {
+        System.out.println("Bienvenido al libro de calificaciones");
+    }
+    
+}
