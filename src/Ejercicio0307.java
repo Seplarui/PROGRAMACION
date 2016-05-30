8aca8f6a8158b835f57dbfa9ae85b6910b0f6fd9
Sebas <seplarui@gmail.com>
2016-05-30 09:27:35 +0200

    Ejercicio0307

---
+++ src/Ejercicio0307.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haplo
+ */
+public class Ejercicio0307 {
+    
+    public static void main(String[] args){
+        
+    for(int i=1;i<=10;i++) {
+        System.out.println("Tabla del "+i);
+        System.out.println("************");
+        for(int j=1;j<=10;j++) {
+            System.out.println(i+"x"+j+"="+j*i);
+        }
+    
+}
+    }
+}
