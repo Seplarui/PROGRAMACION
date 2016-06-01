3eba0382dfd2c5b84bf2619adde57f6b220c15d6
Sebas SEbas <seplarui@gmail.com>
2016-06-01 18:19:03 +0200

    vectorLimites.java

---
+++ src/vectorLimites.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sepla
+ */
+public class vectorLimites {
+    
+    public static void main(String[] args) {
+        int v[]=new int[2];
+        try {
+            v[12]=3;
+            
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+}
