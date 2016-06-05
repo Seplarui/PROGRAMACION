5bdb04b94358e358ca85a62ed70e9786013231ff
Sebas SEbas <seplarui@gmail.com>
2016-06-05 11:04:46 +0200

    filereader01.java

--- src/filereader01.java
+++ src/filereader01.java
@@ -8,6 +8,38 @@
  *
  * @author sepla
  */
+
+import java.io.*;
+import java.util.StringTokenizer;
 public class filereader01 {
+    public int cuentaPalabras(String fichero) {
+        int contador=0;
+        try{ 
+            String s;
+            File fe= new File(fichero);
+            FileReader fr=new FileReader(fe);
+            BufferedReader f = new BufferedReader(fr);
+            while((s=f.readLine())!=null) {
+                StringTokenizer str= new StringTokenizer(s);
+                contador+=str.countTokens();
+            }
+            f.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: No existe"+fichero);
+        } catch (IOException e) {
+            
+        } catch (Throwable e) {}
+        return contador;
+    }
+    
+    public static void main(String [] args) {
+        filereader01 c=new filereader01();
+        int d= c.cuentaPalabras(args[0]);
+        if(d!=0) {
+            System.out.println("Fichero: "+args[0]);
+            System.out.println("Palabras: "+d);
+        }
+            
+    }
     
 }
