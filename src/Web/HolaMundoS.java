6d54d8b3b24504f1741b0fb611183bdb1ee517fd
Sebas SEbas <seplarui@gmail.com>
2016-06-05 13:14:44 +0200

    HolaMundoS

--- src/Web/HolaMundoS.java
+++ src/Web/HolaMundoS.java
@@ -3,12 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Web;
 
 /**
  *
  * @author sepla
  */
-public class HolaMundoS {
+import javax.swing.*;
+
+public class HolaMundoS  extends JFrame {
+    public HolaMundoS() {
+        super("Hola Mundo");
+        JLabel label=new JLabel("Hola Mundo");
+        this.getContentPane().add(label);
+        setSize(200,100);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) {
+        new HolaMundoS();
+    }
+    
+    
+    
+    
     
 }
