f51c80785340a1960d5be2711c701fcc5975b7c1
Sebas SEbas <seplarui@gmail.com>
2016-06-05 11:27:45 +0200

    todoamayusculas.java

--- src/TodoAMayusculas.java
+++ src/TodoAMayusculas.java
@@ -8,6 +8,62 @@
  *
  * @author sepla
  */
+import java.io.*;
 public class TodoAMayusculas {
+    public static void main(String[] arguments) {
+        TodoAMayusculas may= new TodoAMayusculas("Ejemplo.txt");
+        may.convertir();
+    }
+    
+    String nombreFuente;
+    
+    TodoAMayusculas(String fuenteArg) {
+        nombreFuente=fuenteArg;
+    }
+    
+    void convertir() {
+        File fuente= new File(nombreFuente);
+        File temp= new File("may"+nombreFuente+".tmp");
+        
+        FileReader fr= new FileReader(fuente);
+        BufferedReader in = new BufferedReader(fr);
+        
+        FileWriter fw=new FileWriter(temp);
+        BufferedWriter out= new BufferedWriter(fw);
+        boolean eof=false;
+        int inChar=0;
+        
+        do {
+            inChar=in.read();
+            System.out.println((char)inChar);
+            System.out.println();
+            if (inChar!=-1) {
+                
+                char outChar=Character.toUpperCase((char)inChar);
+                out.write(outChar);
+                System.out.println();
+                
+
+            } else {
+                eof=true;
+            }
+        } while (!eof);
+        in.close();
+        out.close();
+        boolean borrado=fuente.delete();
+        if(borrado) {
+            temp.renameTo(fuente);
+        }
+        
+        
+        
+        
+    }catch (IOException e) {
+    
+}catch (SecurityException se) {
+}
+
+}
+
     
 }
