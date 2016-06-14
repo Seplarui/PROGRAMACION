3b78aae147eb8da2addc1ddc8e3aeeb4a816cfb4
Sebas SEbas <seplarui@gmail.com>
2016-06-14 21:10:49 +0200

    Ejercicios Básicos

---
+++ EjerciciosBasicos/EjerciciosBasicos/src/ejerciciosbasicos/Ejercicio2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosbasicos;
+/*
+ * Hacer un programa que muestre los números del 1 al 100.
+ * Con FOR
+ * Ejemplo: 1,2, ... 100
+ */
+
+
+public class Ejercicio2 {
+    
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=100;i++) {
+            System.out.println(i);
+        }
+        
+        
+    }
+    
+}
---
+++ EjerciciosBasicos/EjerciciosBasicos/src/ejerciciosbasicos/EjerciciosBasicos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosbasicos;
+/*
+ * Hacer un programa que muestre los números del 1 al 100.
+ * Con while
+ * Ejemplo: 1,2, ... 100
+ */
+
+public class EjerciciosBasicos {
+
+  
+    public static void main(String[] args) {
+        
+        int i=1;
+        while (i<=100) {
+            
+            System.out.println(i);
+            i++;
+        }
+
+
+
+    }
+    
+}
