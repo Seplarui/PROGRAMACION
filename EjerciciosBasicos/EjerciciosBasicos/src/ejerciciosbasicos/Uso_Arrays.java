e60a7f036f76cdbc13bcb1f96ace150617ffa20b
Sebas SEbas <seplarui@gmail.com>
2016-06-15 11:26:02 +0200

    Uso de Arrays

---
+++ EjerciciosBasicos/EjerciciosBasicos/src/ejerciciosbasicos/Uso_Arrays.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosbasicos;
+
+public class Uso_Arrays {
+    
+    public static void main(String[] args) {
+        
+/*        int [] mi_matriz=new int[5];
+        
+        mi_matriz[0]=5;
+        mi_matriz[1]=4;
+        mi_matriz[2]=21;
+        mi_matriz[3]=25;
+        mi_matriz[4]=35;*/
+
+        int [] mi_matriz={5,33,-23,4,52};
+        
+
+
+        
+        
+        for(int i=0;i<mi_matriz.length;i++) {
+            
+            System.out.println(mi_matriz[i]);
+        }
+        
+        
+    }
+    
+}
